c013be6e7ac78807629e1a4185af81ba9698dd25
Adam Ovadia <adam@aovadia.com>
2024-06-19 16:57:52 -0400

    Create Insertion Sort method. Support for ASC and DESC sorting

---
+++ InsertionSort.py
@@ -0,0 +1,47 @@
+import random
+
+class InsertionSort:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def sortASC(data):
+        for j in range(1, len(data)):
+            InsertionSort.insert(data, j)
+        return data
+
+    @staticmethod
+    def sortDSC(data):
+        for j in range(1, len(data)):
+            InsertionSort.insertDSC(data, j)
+        return data
+    
+    @staticmethod
+    def insertDSC(data, j):
+        i = j - 1
+        while i >= 0 :
+            if data[i] < data[i + 1]:
+                InsertionSort.swap(data, i, i+1)
+                i -= 1
+            else: break       
+
+
+    @staticmethod
+    def insert(data, j):
+        i = j - 1
+        while i >= 0 :
+            if data[i] > data[i + 1]:
+                InsertionSort.swap(data, i, i+1)
+                i -= 1
+            else: break
+
+    @staticmethod
+    def swap(data, val1, val2):
+        left_pos = data[val1]
+        right_pos = data[val2]
+        data[val2] = left_pos
+        data[val1] = right_pos
+
+data = [1,34,5,2,6,34,5,7,2,2]
+print(InsertionSort.sortDSC(data))
