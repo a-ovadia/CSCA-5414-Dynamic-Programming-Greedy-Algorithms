ed5a7d7f3a7b0d84753c865a0a78b6056412d01a
Adam Ovadia <adam@aovadia.com>
2024-06-28 13:45:57 -0400

    add docstrings

--- HashTable/LinkedList.py
+++ HashTable/LinkedList.py
@@ -6,9 +6,16 @@ class LinkedList:
         self.head = None
 
     def insert(self, key, value):
+        """" Inserts new element to the end of the list"""
+        
+        # Initialze new Node
         newNode = Node(key, value)
+
+        # Check if Head is empty
         if not self.head:
             self.head = newNode
+
+        # Else iterate through linked list to find last element to append new Node    
         else:
             current = self.head
             while current.next:
@@ -16,14 +23,18 @@ class LinkedList:
                     current.value = value
                     return True
                 current = current.next
-
+            
+            # If key exists already, modify the value to the new value
             if current.key == key:
                 current.value = value
+            
             else:
                 current.next = newNode
+
         return True
     
     def get(self, key):
+        """ Returns value of a Node of specified key """
         current = self.head
         while current:
             if current.key == key:
@@ -32,11 +43,15 @@ class LinkedList:
         return None
     
     def remove(self, key):
+        """Remove a key from the linked list"""
+
+        # Check if linked list is empty
         if not self.head:
             return None
         
         value_to_delete = None
 
+        # Check if the key is at the head
         if self.head.key == key:
             value_to_delete = self.head.value
             self.head = self.head.next
@@ -44,6 +59,7 @@ class LinkedList:
         
         current = self.head
 
+        # Iterate through linked list to find key
         while current.next:
             if current.next.key == key:
                 value_to_delete = current.next.value
